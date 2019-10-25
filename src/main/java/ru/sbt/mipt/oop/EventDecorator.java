7c85ba9c303509ecd2c65b70f81937bfe685b1e2
sergeynikiforov761 <nikiforov.sv@phystech.edu>
2019-10-25 19:09:54 +0300

    homework-3 update

--- src/main/java/ru/sbt/mipt/oop/EventDecorator.java
+++ src/main/java/ru/sbt/mipt/oop/EventDecorator.java
@@ -1,15 +1,27 @@
 package ru.sbt.mipt.oop;
 
 
+import ru.sbt.mipt.oop.alarm.Alarm;
+import ru.sbt.mipt.oop.alarm.AlarmState;
+import ru.sbt.mipt.oop.alarm.AlarmType;
+
 public class EventDecorator {
 
     private ProcessingEvent processingEvent;
+    private Alarm alarm;
 
-    public EventDecorator(ProcessingEvent processingEvent) {
+    public EventDecorator(ProcessingEvent processingEvent, Alarm alarm) {
         this.processingEvent = processingEvent;
+        this.alarm = alarm;
     }
 
     public void processEvent() {
-        processingEvent.processEvent();
+        if (alarm.getClassAvailability() == AlarmType.DEACTIVATION) {
+            processingEvent.processEvent();
+        } else if (alarm.getClassAvailability() == AlarmType.ALARM) {
+            System.out.println("Alarm");
+        } else {
+            alarm.changeState(new AlarmState(), alarm.getCode());
+        }
     }
 }
